c2265945d479602302a531142f4d3631090d0e2f
alan5123 <alanparker5123@gmail.com>
2020-03-02 19:06:14 -0500

    changed password validation

--- hack/src/components/SignInAP.js
+++ hack/src/components/SignInAP.js
@@ -47,8 +47,7 @@ const SignInForm = withFormik({
   
    password: Yup.string()
    .required('No password provided.') 
-   .min(8, 'Password is too short - should be 8 chars minimum.')
-   .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+   
   
   
  
